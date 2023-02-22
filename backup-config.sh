117ff1532cd28a793592f0e700686ca82a4c46e8
freande <75705976+freande@users.noreply.github.com>
2023-02-22 22:28:27 +0100

    fix typo

--- backup-config.sh
+++ backup-config.sh
@@ -2,7 +2,7 @@
 
 CUR_DIR="$(dirname "$0")"
 
-sudo cp -r ~/.config/alacritty/.alacritty.yml $CUR_DIR/files/.config/alacritty/.alacritty.yml
+sudo cp -r ~/.config/alacritty/alacritty.yml $CUR_DIR/files/.config/alacritty/.alacritty.yml
 sudo cp -r ~/.config/awesome/rc.lua $CUR_DIR/files/.config/awesome/rc.lua
 sudo cp -r ~/.config/awesome/themes/default $CUR_DIR/files/.config/awesome/themes/
 sudo cp -r ~/.config/rofi/config.rasi $CUR_DIR/files/.config/rofi/config.rasi
