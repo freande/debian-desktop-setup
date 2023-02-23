2e60706c4f4bda37ec4de0e212050b032df1c6c3
freande <75705976+freande@users.noreply.github.com>
2023-02-23 21:05:27 +0100

    Add Kitty conf

--- backup-config.sh
+++ backup-config.sh
@@ -6,4 +6,5 @@ sudo cp -r $HOME/.config/awesome/. $CUR_DIR/files/dotconfig/awesome/
 sudo cp -r $HOME/.config/rofi/. $CUR_DIR/files/dotconfig/rofi/
 sudo cp $HOME/.config/gtk-3.0/settings.ini $CUR_DIR/files/dotconfig/gtk-3.0/settings.ini
 sudo cp $HOME/.gtkrc-2.0 $CUR_DIR/files/.gtkrc-2.0
+sudo cp $HOME/.config/kitty/kitty.conf $CUR_DIR/dotconfig/kitty/kitty.conf
 sudo cp $HOME/.zshrc $CUR_DIR/files/.zshrc
