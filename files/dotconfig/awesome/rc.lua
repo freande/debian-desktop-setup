3ff9bb94c67626ad0c3ae8a719fe63b480c44c03
freande <75705976+freande@users.noreply.github.com>
2023-03-04 02:38:03 +0100

    Remove useless gaps

--- files/dotconfig/awesome/rc.lua
+++ files/dotconfig/awesome/rc.lua
@@ -494,7 +494,6 @@ awful.rules.rules = {
             buttons = clientbuttons,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-            size_hints_honor = false
         }
     },
 
