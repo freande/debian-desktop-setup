70550bcb058d24bf625e784f6681de08ad6c8f25
freande <75705976+freande@users.noreply.github.com>
2023-03-07 01:52:26 +0100

    Add font config

--- copy-config.sh
+++ copy-config.sh
@@ -8,6 +8,7 @@ sudo cp $CUR_DIR/files/.gtkrc-2.0 $HOME/.gtkrc-2.0
 sudo cp $CUR_DIR/files/.zshrc $HOME/.zshrc
 mkdir -p $HOME/.dircolors
 sudo cp $CUR_DIR/files/dircolors.theme $HOME/.dircolors/dircolors.theme
+sudo cp $CUR_DIR/files/.fonts.conf $HOME/.fonts.conf
 
 sudo chown -R $USERNAME:$USERNAME /home/$USERNAME
 chmod -R +x $HOME/.config/rofi/scripts
